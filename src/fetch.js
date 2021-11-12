219e4d4b51c771e829c01d52f726a9739ae624f9
Fondem Junior <fondemjunior@yahoo.com>
2021-11-12 17:21:15 +0100

    Fix API issues

--- src/fetch.js
+++ src/fetch.js
@@ -1,14 +1,20 @@
-// const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+const fetch = require('node-fetch');
 
-export default async function leaderBoard() {
-  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+
+const leaderBoard = () => {
+  fetch(baseURL, {
     method: 'POST',
+    body: JSON.stringify({
+      name: 'fifa',
+    }),
     headers: {
-      'Content-type': 'application/json;',
+      'Content-type': 'application/json; charset=UTF-8',
     },
-    body: JSON.stringify({ name: 'my first game' }),
   })
     .then((response) => response.json())
-    .then((json) => json)
+    .then((json) => localStorage.setItem('gameId', JSON.stringify(json.result.split(' ')[3])))
     .catch((error) => new Error('Error:', error));
-}
+};
+
+export default leaderBoard;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
 import leaderBoard from './fetch.js';
 
-const leaderData = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/2522casjsbfo/scores/';
 const displayGame = document.querySelector('.list');
 const form = document.querySelector('.form');
 const userName = document.querySelector('.input-name');
@@ -9,7 +8,8 @@ const userScore = document.querySelector('.input-score');
 const refreshBtn = document.querySelector('.refresh-button');
 
 const postData = async (playerName, gamerScore) => {
-  const response = await fetch(leaderData, {
+  const id = JSON.parse(localStorage.getItem('gameId'));
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json; charset=UTF-8',
@@ -32,12 +32,14 @@ form.addEventListener('submit', (e) => {
 });
 
 const getData = async () => {
-  const refreshData = await fetch(leaderData);
+  const id = JSON.parse(localStorage.getItem('gameId'));
+  const refreshData = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`);
   const refreshJsonData = await refreshData.json();
   return refreshJsonData;
 };
 
 const refreshScores = () => {
+  
   getData().then((data) => {
     displayGame.innerHTML = '';
     data.result.forEach((data) => {
@@ -53,6 +55,9 @@ const refreshScores = () => {
 
 refreshBtn.addEventListener('click', refreshScores);
 
-leaderBoard();
+if (localStorage.getItem('gameId')) {
+  console.log( localStorage.getItem('gameId') )
+}
+else { leaderBoard(); }
 
-// window.onload = refreshScores;
+window.onload = refreshScores;
